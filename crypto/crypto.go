bf3927280f7f596530e3a8c7903356d6d73d98f7
Aksel Wullum-Vestad <aksel@aksels.net>
2025-03-10 09:55:35 +0700

    add sepereate crypto package

---
+++ crypto/crypto.go
@@ -0,0 +1,59 @@
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+)
+
+type Encrypter interface {
+	Encrypt([]byte) []byte
+}
+
+type Decrypter interface {
+	Decrypt([]byte) []byte
+}
+
+type EncryptDecrypter interface {
+	Encrypter
+	Decrypter
+}
+
+type symetricEncryptor struct {
+	aead cipher.AEAD
+}
+
+func NewSymetricEncryprion(secretKey []byte) (symetricEncryptor, error) {
+	aes, err := aes.NewCipher(secretKey)
+	if err != nil {
+		return symetricEncryptor{}, err
+	}
+
+	gcm, err := cipher.NewGCM(aes)
+	if err != nil {
+		return symetricEncryptor{}, err
+	}
+
+	return symetricEncryptor{gcm}, nil
+}
+
+func (e symetricEncryptor) Encrypt(plaintext []byte) ([]byte, error) {
+	nonce := make([]byte, e.aead.NonceSize())
+	_, err := rand.Read(nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	return e.aead.Seal(nonce, nonce, plaintext, nil), nil
+}
+
+func (e symetricEncryptor) Decrypt(chiper []byte) ([]byte, error) {
+	if len(chiper) < e.aead.NonceSize() {
+		return nil, errors.New("chipertext missing nonce")
+	}
+
+	nonce := chiper[:e.aead.NonceSize()]
+	chipertext := chiper[e.aead.NonceSize():]
+	return e.aead.Open(nil, nonce, chipertext, nil)
+}
