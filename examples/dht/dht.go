7bc0c514a787133514566b6f37cf984b71c99a84
FluffyKebab <aksel@aksels.net>
2025-04-29 15:34:03 +0200

    update dht example

--- examples/dht/dht.go
+++ examples/dht/dht.go
@@ -9,8 +9,8 @@ import (
 	"os"
 	"strings"
 
+	"github.com/FluffyKebab/pearly/kademila"
 	"github.com/FluffyKebab/pearly/node/basic"
-	"github.com/FluffyKebab/pearly/node/kadmilla"
 	"github.com/FluffyKebab/pearly/peer"
 	"github.com/FluffyKebab/pearly/transport/encrypted"
 	"github.com/FluffyKebab/pearly/transport/tcp"
@@ -23,25 +23,26 @@ func main() {
 }
 
 func run() error {
-	port, bootsapAddr := getUserInput()
+	port, bootstrapAddr := getUserInput()
 
-	// First we initlize the transport the node is going to use to comunicate
-	// with other nodes. In this case we are using encrypted tcp. In addtion to
-	// obfuscatig the data, this transport also gives our node an ID by
-	// hasing the public key of the transport. This ID is both used by the dht
-	// and in the transport to validate that we are comunicating with the right
-	// node. When the transport upgrades the connection, it recives both the
-	// ID and the public key of the peer. It then validates that the ID is
-	// the hash of the public key. If not the connection is discarded.
+	// First we initialize the transport the node is going to use to communicate
+	// with other nodes. In this case we are using encrypted tcp. In addition to
+	// obfuscating the data, this transport also gives our node an ID by
+	// hasing the public key the transport uses for encryption. This ID is both
+	// used by the dht and in the transport to validate that we are communicating
+	// with the correct node. When the transport upgrades the connection, it
+	// receives both the ID and the public key of the peer. It then validates
+	// that the ID is the hash of the public key. If not the connection is
+	// discarded.
 	transport, err := encrypted.NewTransport(tcp.New(port))
 	if err != nil {
 		return err
 	}
 
-	// Next we initlize and run a node that uses the transport we just created.
-	// We recive all errors that result from handling remote connections in the
+	// Next we initialize and run a node that uses the transport we just created.
+	// We receive all errors that result from handling remote connections in the
 	// error chan. The node is also responsible for protocol muxing. E.G.
-	// making sure that the right protocol handler is called when we recive
+	// making sure that the right protocol handler is called when we receive
 	// a new connection.
 	node := basic.New(transport, transport.ID())
 	errChan, err := node.Run(context.Background())
@@ -55,28 +56,28 @@ func run() error {
 		}
 	}()
 
-	// Using the node we created, we can initlize a kadmila dht.
-	dht := kadmilla.New(node)
-	if bootsapAddr != "" {
-		// The bootsrap proces is how we join an already created network. This
-		// implementaion is simple, and consists of only two steps: first we
+	// Using the node we created, we can initialize a kadmila dht.
+	dht := kademila.New(node)
+	if bootstrapAddr != "" {
+		// The bootstrap process is how we join an already created network. This
+		// implementation is simple, and consists of only two steps: first we
 		// try to find a key with the same value as our own node. If there is
-		// space in the bootsrap nodes peer store, it will add us as a peer.
+		// space in the bootstrap nodes peer store, it will add us as a peer.
 		// Lastly we add the node to our own peerstore.
-		err = dht.Bootstrap(context.Background(), peer.New(nil, bootsapAddr))
+		err = dht.Bootstrap(context.Background(), peer.New(nil, bootstrapAddr))
 		if err != nil {
 			return fmt.Errorf("bootsrap: %w", err)
 		}
 	}
 
 	for {
-		if err := doComand(dht); err != nil {
+		if err := doCommand(dht); err != nil {
 			fmt.Println(err.Error())
 		}
 	}
 }
 
-func doComand(dht kadmilla.DHT) error {
+func doCommand(dht kademila.DHT) error {
 	fmt.Println("(add/get) (value/key)")
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
--- examples/dht/go.mod
+++ examples/dht/go.mod
@@ -2,7 +2,7 @@ module github.com/FluffyKebab/pearly/examples/dhtmsg
 
 go 1.23.4
 
-require github.com/FluffyKebab/pearly v0.0.0-20250127154912-e58c65509f5c
+require github.com/FluffyKebab/pearly v0.0.0-20250429131748-9b90ade38507
 
 require (
 	github.com/multiformats/go-multistream v0.6.0 // indirect
